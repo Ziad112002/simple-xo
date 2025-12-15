21809fc59d4e2f02a7cf8eb03d0b47f5ae4c6938
ziad <mqpz596@gmail.com>
2025-12-15 20:36:17 +0200

    back to first color

--- lib/utils/app_color.dart
+++ lib/utils/app_color.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColor {
   static const Color lightBlue = Color(0xff00D2FF);
-  static const Color blue = Color(0xffb3b3b8);
+  static const Color blue = Color(0xff3A7BD5);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 }
-//update blue color
-
