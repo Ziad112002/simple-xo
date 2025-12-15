205321a0c59e9c9859cfd912849004cdc5db9409
ziad <mqpz596@gmail.com>
2025-12-15 18:20:23 +0200

    Learn merge command

--- lib/utils/app_color.dart
+++ lib/utils/app_color.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColor {
-  static const Color lightBlue = Color(0xffc63737);
-  static const Color blue = Color(0xff92b0df);
+  static const Color lightBlue = Color(0xff00D2FF);
+  static const Color blue = Color(0xff3A7BD5);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 }
+//learn merge command
