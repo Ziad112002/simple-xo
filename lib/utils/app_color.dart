6ad3363080156396d5569b92c702022d6a18ee96
ziad <mqpz596@gmail.com>
2025-12-15 20:37:52 +0200

    add red color

--- lib/utils/app_color.dart
+++ lib/utils/app_color.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColor {
-  static const Color lightBlue = Color(0xff00D2FF);
+  static const Color lightBlue = Color(0xffd51b0f);
   static const Color blue = Color(0xff3A7BD5);
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 }
+// add red color
